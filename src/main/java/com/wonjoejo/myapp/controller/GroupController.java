57013fc7250ae40a49b4a0104da8ea1ff581cab2
jayoung <jahr7209@naver.com>
2021-11-05 18:58:37 +0900

    Group 만드는 중

---
+++ src/main/java/com/wonjoejo/myapp/controller/GroupController.java
@@ -0,0 +1,19 @@
+package com.wonjoejo.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@NoArgsConstructor
+
+@RequestMapping("/group")
+@Controller
+public class GroupController {
+
+	
+	
+	
+}
