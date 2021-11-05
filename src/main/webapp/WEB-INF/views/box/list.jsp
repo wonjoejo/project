780870cc020add0c197cd8f2f38f78708ea73ea4
heewon <seoheewon.dev@gmail.com>
2021-11-05 18:40:54 +0900

    Box Test Mapper, Service 완성 및 Controller Test 작업중

--- src/main/webapp/WEB-INF/views/box/list.jsp
+++ src/main/webapp/WEB-INF/views/box/list.jsp
@@ -1,3 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+		 pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <html>
@@ -12,6 +14,10 @@
 <c:forEach items="${list}" var="box">
 	${box}
 </c:forEach>
+
+${param.result}
+
+<button onclick="location.href='${pageContext.request.contextPath}/box/create'">박스 생성</button>
 
 </body>
 </html>
--- src/test/java/com/wonjoejo/myapp/controller/BoxControllerTests.java
+++ src/test/java/com/wonjoejo/myapp/controller/BoxControllerTests.java
@@ -1,10 +1,43 @@
 package com.wonjoejo.myapp.controller;
 
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
 
 @Log4j2
 @NoArgsConstructor
+
+@WebAppConfiguration
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration({
+        "file:src/main/webapp/WEB-INF/spring/root-context.xml",
+        "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
+})
 public class BoxControllerTests {
+
+    @Setter(onMethod_= { @Autowired})
+    private WebAppConfiguration ctx;
+
+    private MockMvc mockMvc;
+    private MockMvcBuilder mockMvcBuilder;
+
+    @Before
+    public void setup() {
+        log.debug("setup() invoked.");
+
+        assert this.ctx != null;
+        log.info("\t+ ctx:{}",this.ctx);
+    } // setup
+
+
 
 }
