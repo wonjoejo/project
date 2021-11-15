37438b54d4c02b9cee63b0c03b8032ed33c8f920
jihyun <harna26@gmail.com>
2021-11-16 02:59:24 +0900

    MemberController 오류
    MemberController 오류

--- src/main/java/com/wonjoejo/myapp/controller/MemberController.java
+++ src/main/java/com/wonjoejo/myapp/controller/MemberController.java
@@ -66,8 +66,7 @@ public class MemberController {
 	
 	// 로그인
 	@PostMapping("/loginPost")
-	public void loginPost(
-			LoginDTO dto, Model model, HttpSession session) throws Exception {
+	public void loginPost(LoginDTO dto, Model model, HttpSession session) throws Exception {
 		log.debug("loginPost({}) invoked.", dto);
 		
 		MemberVO member=this.service.login(dto);
