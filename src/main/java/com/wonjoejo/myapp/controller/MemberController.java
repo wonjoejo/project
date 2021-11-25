57ab3af4bfa23c185ee3e0a091944b4bb6e3a1b0
heewon <seoheewon.dev@gmail.com>
2021-11-26 02:31:58 +0900

    mention 기능 memo 부분에 추가
    mention 기능 memo 부분에 추가

--- src/main/java/com/wonjoejo/myapp/controller/MemberController.java
+++ src/main/java/com/wonjoejo/myapp/controller/MemberController.java
@@ -1,27 +1,22 @@
 package com.wonjoejo.myapp.controller;
 
-import java.util.Date;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+import com.google.gson.Gson;
 import com.wonjoejo.myapp.domain.LoginDTO;
 import com.wonjoejo.myapp.domain.MemberDTO;
 import com.wonjoejo.myapp.domain.MemberVO;
 import com.wonjoejo.myapp.service.MemberService;
-
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 @Log4j2
 @NoArgsConstructor
@@ -243,18 +238,31 @@ public class MemberController {
 
 	}
 
-	
+
 	@GetMapping("/myPage")
 	public void edit(String member_id, Model model) {
 		log.debug("myPage invoked.");
-		
+
 		MemberVO member = this.service.getMember(member_id);
 		log.info("\t+ member: {}", member);
-		
-		model.addAttribute("member",member);
-		
-	}
-	
 
-    
+		model.addAttribute("member", member);
+
+	}
+
+	@GetMapping(value = "/profile", produces = "application/json; charset=utf8")
+	@ResponseBody
+	public String profile(String member_id) {
+		log.debug("profile({}) invoked.", member_id);
+
+		MemberVO member = this.service.getMember(member_id);
+		log.info("\t+ member: {}", member);
+
+		Gson gson = new Gson();
+
+		return gson.toJson(member);
+
+	} // profile
+
+
 } // end class
